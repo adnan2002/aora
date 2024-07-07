18a10065bc0457df1194d192fe3405b5417605e6
Adnan <qassimhassan7@gmail.com>
2024-07-07 19:38:47 +0300

    some assets and stuff

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">This is the app</Text>
+      <Text className="text-3xl font-pblack">This is the app</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color:'blue'}}>Go to profile</Link>
     </View>
